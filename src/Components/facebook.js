4a98dbd0dbd14d9152c3cacf1e84f7810a0469d5
ali1129js <ali1129js@gmail.com>
2018-06-03 10:48:58 +0200

    clean up

---
+++ src/Components/facebook.js
@@ -0,0 +1,6 @@
+/**
+ * @Author: Ali
+ * @Date:   2018-06-03T10:47:42+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-06-03T10:47:43+02:00
+ */
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * @Author: Ali
+ * @Date:   2018-06-03T10:46:48+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-06-03T10:46:52+02:00
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
